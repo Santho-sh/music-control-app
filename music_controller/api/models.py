27b90e3a83b0257762b24c26c465fc5ab035552e
Santhosh <santhoshkalish333@gmail.com>
2023-03-16 21:38:37 +0530

    rest_framework init

--- music_controller/api/models.py
+++ music_controller/api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+import random
+import string
 
-# Create your models here.
+
+def generate_code():
+    length = 6
+
+    while True:
+        new_code = ''.join(random.choices(string.ascii_uppercase +
+                                          string.digits, k=length))
+        if Room.objects.filter(code=new_code).count() == 0:
+            return new_code
+
+
+class Room(models.Model):
+    host = models.CharField(max_length=50, unique=True)
+    code = models.CharField(max_length=8, unique=True)
+    can_pause = models.BooleanField(default=False)
+    skip_votes = models.IntegerField(default=1)
+    created = models.DateTimeField(auto_now_add=True)
---
+++ music_controller/api/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Room
+
+
+class Roomserializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('id', 'code', 'host', 'can_pause', 'skip_votes', 'created')
